82949d530e7b8e7f35e3bb036e8eeb36c4a12f45
Tristan <thrawst88@gmail.com>
2015-10-09 18:35:00 -0600

    fix #2 added support for shortform hex colors

--- parse/hex.js
+++ parse/hex.js
@@ -1,4 +1,9 @@
 function hex(hex) {
+  if (hex.length === 4) {
+    hex = '#' + hex[1] + hex[1] +
+      hex[2] + hex[2] + 
+      hex[3] + hex[3];
+  }
   return [
     parseInt(hex.substring(1,3), 16),
     parseInt(hex.substring(3,5), 16),
