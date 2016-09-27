d47cbf0ff37162596fa104207e5634594e66b0a7
0tho <marcelo.lopes.lotufo@gmail.com>
2016-09-26 23:44:48 -0300

    Add support for #RGBA and #RRGGBBAA

--- parse/hex.js
+++ parse/hex.js
@@ -1,14 +1,32 @@
 function hex(hex) {
-  if (hex.length === 4) {
-    hex = '#' + hex.charAt(1) + hex.charAt(1) +
-      hex.charAt(2) + hex.charAt(2) + 
-      hex.charAt(3) + hex.charAt(3);
+  // # FFF || # FF FF FF
+  if (hex.length % 3 === 1) {
+    if (hex.length === 4) {
+      hex = '#' + hex.charAt(1) + hex.charAt(1) +
+        hex.charAt(2) + hex.charAt(2) +
+        hex.charAt(3) + hex.charAt(3);
+    }
+    return [
+      parseInt(hex.substring(1,3), 16),
+      parseInt(hex.substring(3,5), 16),
+      parseInt(hex.substring(5,7), 16)
+    ];
   }
-  return [
-    parseInt(hex.substring(1,3), 16),
-    parseInt(hex.substring(3,5), 16),
-    parseInt(hex.substring(5,7), 16)
-  ];
+  // # FFFF || # FF FF FF FF
+  else if (hex.length % 4 === 1) {
+    if (hex.length === 5) {
+      hex = '#' + hex.charAt(1) + hex.charAt(1) +
+        hex.charAt(2) + hex.charAt(2) +
+        hex.charAt(3) + hex.charAt(3) +
+        hex.charAt(4) + hex.charAt(4);
+    }
+    return [
+      parseInt(hex.substring(1,3), 16),
+      parseInt(hex.substring(3,5), 16),
+      parseInt(hex.substring(5,7), 16),
+      parseFloat((parseInt(hex.substring(7,9), 16) / 255).toFixed(2))
+    ];
+  }
 }
 
 module.exports = hex;
